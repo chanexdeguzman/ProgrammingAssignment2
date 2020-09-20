25cd448589be05247458f8d92ee6016a34762341
chanexdeguzman <68182649+chanexdeguzman@users.noreply.github.com>
2020-09-20 14:32:38 +0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  makeCacheMatrix <- function(x = matrix()) { ## define the argument
+    inv <- NULL                             ## initialize inv as NULL; will hold value of matrix inverse 
+    set <- function(y) {                    ## define the set function to assign new 
+      x <<- y                             ## value of matrix in parent environment
+      inv <<- NULL                        
+    }
+    get <- function() x                     ## returns value of the matrix argument
+    
+    setinverse <- function(inverse) inv <<- inverse  ## assigns value of the inverse
+    getinverse <- function() inv                     ## gets the value of the inverse
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse) 
 
 }
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
